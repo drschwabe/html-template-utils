ef40abaa1729ee4156a5b47c71dc2032889eef7f
Derrick Schwabe <drschwabe@fastmail.com>
2024-12-23 16:39:24 -0700

    Remove uhtml utils, rename renderVanilla to render

--- html-template-utils.js
+++ html-template-utils.js
@@ -1,9 +1,7 @@
 const { log, warn } = console 
-const { html, render: uhtmlSSRrender } = require('uhtml-ssr')
-const htmlRaw = str => html([str])
 const $j = require('jquery')  
 
-const renderVanilla = require('./render-vanilla')
+const render = require('./render-vanilla')
 
 const isBrowser = () => typeof window !== 'undefined' 
   && typeof window.document !== 'undefined'
@@ -16,24 +14,11 @@ if(isBrowser()) {
   }
 }
 
-let render 
-
-//Browser uhtml(v3 and v4) is sometimes inconsistent with uhtml-ssr on server
-//so we just use uhtml-ssr on client too: 
-if(isBrowser()) {
-  render = (where, what) => {
-    const string = uhtmlSSRrender(String, what)
-    return where.innerHTML = string
-  }
-} else {
-  render = uhtmlSSRrender
-}
 
 const css = (...classes) => 
   classes.flat().filter(Boolean).join(' ')
 
-
-const Grow = html`<span class="grow"></span>` 
+const Grow = `<span class="grow"></span>` 
 
 const isEditing = () => {
   const activeElement = document.activeElement;
@@ -47,8 +32,6 @@ const isEditing = () => {
   return false 
 } 
 
- 
-
-module.exports = { css, html, htmlRaw, render, $j, isBrowser, Grow, 
-  isEditing, renderVanilla
+module.exports = { css, render, $j, isBrowser, Grow, 
+  isEditing
 }
